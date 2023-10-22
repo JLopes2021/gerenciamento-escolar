5bb659c7001c213550f43b252aeec4b98c44c0d1
JLopes2021 <jeffinho.je8@gmail.com>
2023-10-22 15:55:29 -0300

    atualizacao-estatisticas

--- src/components/infos/Turmas.js
+++ src/components/infos/Turmas.js
@@ -1,32 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Badge from 'react-bootstrap/Badge';
 import ListGroup from 'react-bootstrap/ListGroup';
 import dados from './DadosTurmas'; // Importe o array de dados
 import styles from './Turmas.module.css';
 
 function DefaultExample() {
+  const [showMore, setShowMore] = useState(false);
+  const [showLess, setShowLess] = useState(false); // Adicionado estado para "Mostrar Menos"
+  const maxItems = 4;
+
+  const handleShowMore = () => {
+    setShowMore(true);
+    setShowLess(true); // Agora que estamos mostrando mais, podemos mostrar menos
+  }
+
+  const handleShowLess = () => {
+    setShowMore(false);
+    setShowLess(false); // Esconde o botão de "Mostrar Menos"
+  }
+
   return (
-    <ListGroup className={styles.turmasList}> 
-      {dados.map((item, index) => (
-        <ListGroup.Item
-          key={index}
-          as="li"
-          className={`d-flex justify-content-between align-items-start ${styles.turmasItem}`}
-        >
-          <div className={`ms-2 me-auto ${styles.turmasContent}`}>
-            <div className={`fw-bold ${styles.subheading}`}>{item.turma}</div>
-            {item.descricao}
-            <div>
-            {item.professor}
+    <div className={styles.container}>
+      <ListGroup className={styles.turmasList}> 
+        {dados.slice(0, showMore ? dados.length : maxItems).map((item, index) => (
+          <ListGroup.Item
+            key={index}
+            as="li"
+            className={`d-flex justify-content-between align-items-start ${styles.turmasItem}`}
+          >
+            <div className={`ms-2 me-auto ${styles.turmasContent}`}>
+              <div className={`fw-bold ${styles.subheading}`}>{item.turma}</div>
+              {item.descricao}
+              <div>
+                {item.professor}
+              </div>
             </div>
-            
-          </div>
-          <Badge bg="primary" pill>
-            <p>Quantidade de alunos: {item.alunos}</p>
-          </Badge>
-        </ListGroup.Item>
-      ))}
-    </ListGroup>
+            <Badge bg="primary" pill>
+              <p>Quantidade de alunos: {item.alunos}</p>
+            </Badge>
+          </ListGroup.Item>
+        ))}
+        {!showMore && dados.length > maxItems && (
+          <button onClick={handleShowMore} className={styles.showMoreButton}>
+            Mostrar Mais
+          </button>
+        )}
+        {showLess && (
+          <button onClick={handleShowLess} className={styles.showMoreButton}>
+            Mostrar Menos
+          </button>
+        )}
+      </ListGroup>
+    </div>
   );
 }
 
