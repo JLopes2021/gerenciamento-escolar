6dd99c6190382a6177e05933457fea9f5a615ca9
JLopes2021 <jeffinho.je8@gmail.com>
2023-10-22 16:16:06 -0300

    atualizacao-stats-menu

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import Navbar from './components/layout/Navbar';
 import Calendario from './components/layout/Calendar';
 import Notificacao from './components/layout/Notifications';
 import Estatisticas from './components/layout/Statistics';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      
-      <Navbar/>
+
+      <Navbar />
       <div>
-      <h1>Funciona</h1>
-      <Calendario/>
-      <Notificacao/>
-      <Estatisticas/>
+        <h1>Funciona</h1>
+        <div className='menu'>
+          <div id="#section1">      <Calendario /></div>
+          <div id="#section2">      <Notificacao />
+          </div>
+          <div id="#section3">      <Estatisticas />
+          </div>
+        </div>
+
       </div>
     </div>
   );
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-function Navbar() {
+function AppNavbar() {
   return (
-    <nav className="Navbar">
-      {<h1>Teste navbar</h1>}
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div className="container">
+        <a className="navbar-brand" href="#home">Meu Aplicativo</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#section1">Seção 1</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#section2">Seção 2</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#section3">Seção 3</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
 
-export default Navbar;
+export default AppNavbar;
